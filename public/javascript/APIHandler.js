6bb76949febf68aa83ead14e55a127572f718147
sherilin <sherilinm96@gmail.com>
2022-04-21 09:11:23 +0200

    done

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,25 +1,32 @@
 class APIHandler {
-  constructor (baseUrl) {
-    this.BASE_URL = baseUrl;
+
+  constructor(url) {
+    this.axiosApp = axios.create({
+      baseURL: url
+    })
   }
 
-  getFullList () {
-
+  getFullList() {
+    return this.axiosApp.get('/characters')
   }
 
-  getOneRegister () {
-
-  }
-
-  createOneRegister () {
-
-  }
-
-  updateOneRegister () {
+  getOneRegister = (characterId) => {
+    return this.axiosApp.get(`/characters/${characterId}`)
 
   }
 
-  deleteOneRegister () {
+  createOneRegister = (characterInfo) => {
+    return this.axiosApp.post(`/characters`, characterInfo)
+
+  }
+
+  updateOneRegister = (characterId, characterInfo) => {
+    return this.axiosApp.put(`/characters/${characterId}`, characterInfo)
+
+  }
+
+  deleteOneRegister = (characterId) => {
+    return this.axiosApp.delete(`/characters/${characterId}`)
 
   }
 }
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,23 +1,55 @@
-const charactersAPI = new APIHandler('http://localhost:8000');
+const charactersAPI = new APIHandler('https://minions-api.herokuapp.com/');
 
 window.addEventListener('load', () => {
   document.getElementById('fetch-all').addEventListener('click', function (event) {
+    charactersAPI
+      .getFullList()
+      .then(({ data }) => {
+        console.log(data)
+        let cartoon = ''
+        data.forEach(elm => {
+          cartoon +=
+            `<div class="character-info">
+            <div class="name">Character Name:${elm.name} </div>
+            <div class="occupation">Character Id:${elm.id}</div>
+            <div class="cartoon">Character Occupation:${elm.occupation}</div>
+            <div class="weapon">Character Wapon:${elm.weapon}</div>
+          </div>`
 
-  });
+          document.querySelector('.characters-container').innerHTML = cartoon
+        });
 
-  document.getElementById('fetch-one').addEventListener('click', function (event) {
-
-  });
-
-  document.getElementById('delete-one').addEventListener('click', function (event) {
-
-  });
-
-  document.getElementById('edit-character-form').addEventListener('submit', function (event) {
-
-  });
-
-  document.getElementById('new-character-form').addEventListener('submit', function (event) {
-
-  });
+      })
+      .catch(err => console.log(err))
+  })
 });
+
+document.getElementById('fetch-one').addEventListener('click', function (event) {
+  const searchInput = document.querySelector('fetchone').value
+  charactersAPI
+    .getOneRegister(searchInput)
+    .then(({ data }) => {
+      let one = `<div class="character-info">
+            <div class="name">Character Name:${data.name} </div>
+            <div class="occupation">Character Id:${data.id}</div>
+            <div class="cartoon">Character Occupation:${data.occupation}</div>
+            <div class="weapon">Character Wapon:${data.weapon}</div>
+          </div>`
+      document.querySelector('.characters-container').innerHTML = one
+    })
+    .catch(err => console.log(err))
+
+});
+
+document.getElementById('delete-one').addEventListener('click', function (event) {
+
+});
+
+document.getElementById('edit-character-form').addEventListener('submit', function (event) {
+
+});
+
+document.getElementById('new-character-form').addEventListener('submit', function (event) {
+
+});
+
